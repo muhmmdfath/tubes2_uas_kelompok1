e018d2d30ecffd577c11658e686e85943f70e2a7
Nurhasan12 <Nurhasan.5889@gmail.com>
2024-12-19 22:00:35 +0700

    feat : convert get fav json to model class ( coding )

--- lib/data/favorite/responsesmodel/favorite_responses_model_get.dart
+++ lib/data/favorite/responsesmodel/favorite_responses_model_get.dart
@@ -1,10 +1,15 @@
-import 'package:meta/meta.dart';
+// import 'package:meta/meta.dart';
 import 'dart:convert';
 
 import 'favorite_responses_model_get.dart';
 
-List<FavoritesResponsesModelGet> favoritesResponsesModelGetFromJson(String str) => List<FavoritesResponsesModelGet>.from(json.decode(str).map((x) => FavoritesResponsesModelGet.fromJson(x)));
-String favoritesResponsesModelGetToJson(List<FavoritesResponsesModelGet> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson()));
+List<FavoritesResponsesModelGet> favoritesResponsesModelGetFromJson(
+        String str) =>
+    List<FavoritesResponsesModelGet>.from(
+        json.decode(str).map((x) => FavoritesResponsesModelGet.fromJson(x)));
+String favoritesResponsesModelGetToJson(
+        List<FavoritesResponsesModelGet> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class FavoritesResponsesModelGet {
   final int id;
@@ -23,32 +28,30 @@ class FavoritesResponsesModelGet {
     required this.image,
   });
 
-  factory FavoritesResponsesModelGet.fromJson(Map<String, dynamic> json) => FavoritesResponsesModelGet(
-    id: json['id'],
-    userId: json['user_id'],
-    imageId: json['image_id'],
-    subId: json['sub_id'],
-    createdAt: json['created_at'],
-    image: Image.fromJson(json['image']),
-  );
+  factory FavoritesResponsesModelGet.fromJson(Map<String, dynamic> json) =>
+      FavoritesResponsesModelGet(
+        id: json['id'],
+        userId: json['user_id'],
+        imageId: json['image_id'],
+        subId: json['sub_id'],
+        createdAt: json['created_at'],
+        image: Image.fromJson(json['image']),
+      );
 
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'user_id': userId,
-    'image_id': imageId,
-    'sub_id': subId,
-    'created_at': createdAt,
-    'image': image.toJson(),
-  };
-
+        'id': id,
+        'user_id': userId,
+        'image_id': imageId,
+        'sub_id': subId,
+        'created_at': createdAt,
+        'image': image.toJson(),
+      };
 }
 
-  class Image {
-    Image();
+class Image {
+  Image();
 
-    factory Image.fromJson(Map<String, dynamic> json) => Image(
-    );
+  factory Image.fromJson(Map<String, dynamic> json) => Image();
 
-    Map<String, dynamic> toJson() => {
-    };
-  }
+  Map<String, dynamic> toJson() => {};
+}
