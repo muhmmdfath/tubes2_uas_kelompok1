c1397f08f2f50e391fa210adb379e580a343a9cb
Muhammad Luthfi Ginarso <32602200002@std.unissula.ac.id>
2024-12-19 14:56:35 +0700

    feet : layouting global_component (searchview) ( coding )

--- lib/core/global_component/searchview.dart
+++ lib/core/global_component/searchview.dart
@@ -1,9 +1,9 @@
+import 'package:flutter/cupertino.dart'; // The import of 'package:flutter/cupertino.dart' is unnecessary because all of the used widgets are in 'package:flutter/material.dart'.
 import 'package:flutter/material.dart';
 
 class SearchViewWidget extends StatelessWidget {
   final Function(String) onSearch;
   final String hintText;
-
   const SearchViewWidget({super.key, required this.onSearch, required this.hintText});
 
   @override
@@ -13,18 +13,18 @@ class SearchViewWidget extends StatelessWidget {
       child: TextField(
         onChanged: onSearch,
         decoration: InputDecoration(
-          prefixIcon: const Icon(Icons.search),
+          prefix: const Icon(Icons.search),
           hintText: hintText,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8.0),
-            borderSide: const BorderSide(color: Colors.grey),
-          ),
+            borderSide: BorderSide(color: Colors.grey), // Use 'const' with the constructor to improve performance.
+          ), // OutlineInputBorder
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8.0),
-            borderSide: const BorderSide(color: Colors.blue),
-          ),
-        ),
-      ),
-    );
+            borderSide: BorderSide(color: Colors.blue), // Use 'const' with the constructor to improve performance.
+          ), // OutlineInputBorder
+        ), // InputDecoration
+      ), // TextField
+    ); // Padding
   }
 }
